5807132016b0ade0af721befa6b2f2fba231c415
dubby <yangzheng03@meituan.com>
2017-09-22 19:16:22 +0800

    add mono and flux

--- src/main/java/cn/dubby/webflux/WelcomeController.java
+++ src/main/java/cn/dubby/webflux/WelcomeController.java
@@ -1,7 +1,17 @@
 package cn.dubby.webflux;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.MonoSink;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Created by teeyoung on 17/9/22.
@@ -11,7 +21,34 @@ public class WelcomeController {
 
     @GetMapping("/")
     public String welcome() {
-        return "Hello World";
+        return "Hello World:";
+    }
+
+    @GetMapping("/hello")
+    public Mono<String> hello(String name) {
+        return Mono.create(new Consumer<MonoSink<String>>() {
+            @Override
+            public void accept(MonoSink<String> stringMonoSink) {
+                stringMonoSink.success("hello, " + name);
+            }
+        });
+    }
+
+    @GetMapping("/hi/{name}")
+    public Flux<List<String>> hi(@PathVariable String name) {
+        return Flux.create(new Consumer<FluxSink<List<String>>>() {
+            @Override
+            public void accept(FluxSink<List<String>> stringFluxSink) {
+
+                List<String> stringList1 = new ArrayList<String>();
+                stringList1.add("hi 1, " + name);
+
+                List<String> stringList2 = new ArrayList<String>();
+                stringList2.add("hi 2, " + name);
+
+                stringFluxSink.next(stringList1).next(stringList2).complete();
+            }
+        });
     }
 
 }
